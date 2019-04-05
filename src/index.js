160cbc5402df9d1e6b7d3b9d3b362842a966c64f
Fernando Daciuk <f.daciuk@gmail.com>
2019-04-05 17:01:08 -0300

    Remove console on production

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import { render } from 'react-dom'
 import App from './app'
 import * as serviceWorker from './serviceWorker'
 
+if (process.env.NODE_ENV === 'production') {
+  function noop () {}
+  Object.keys(console).forEach(c => {
+    console[c] = noop
+  })
+}
+
 render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change

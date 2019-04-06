f3368aecc405d29edfe458b95b6a4737cf65038f
Fernando Daciuk <f.daciuk@gmail.com>
2019-04-06 08:05:31 -0300

    Get event name from cache

--- src/hooks/event-data.js
+++ src/hooks/event-data.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { get, set } from 'idb-keyval'
 import { subjectsIssue } from 'utils'
 import { http, to } from 'utils'
 
@@ -6,15 +7,42 @@ function useEventData (issue) {
   const [eventName, setEventName] = useState('...')
   const issueUrl = subjectsIssue.url(issue)
 
+  async function fetchData () {
+    const [error, { data }] = await to(http.get(subjectsIssue.issueUrlApi(issue)))
+    if (error) {
+      console.log('error:', error)
+      return
+    }
+
+    setSubjectsOnStateAndIDB(
+      data.title.replace('Temas de palestras para a', ''),
+      { shouldUpdateCache: true }
+    )
+  }
+
+  function setSubjectsOnStateAndIDB (eventName, { shouldUpdateCache } = {}) {
+    console.log('set eventName on state')
+    setEventName(eventName)
+
+    if (shouldUpdateCache) {
+      console.log('update subjects on cache')
+      set(`${issue}-eventName`, eventName)
+    }
+  }
+
   useEffect(() => {
     async function effect () {
-      const [error, { data }] = await to(http.get(subjectsIssue.issueUrlApi(issue)))
-      if (error) {
-        console.log('error:', error)
-        return
+      const [errorGettingEventData, subjectsFromCache] = await to(get(`${issue}-eventName`))
+      if (errorGettingEventData || !subjectsFromCache) {
+        console.log('errorGettingEventData?', errorGettingEventData)
+        console.log('Does not have eventData from cache :\\', subjectsFromCache)
+        return fetchData()
       }
 
-      setEventName(data.title.replace('Temas de palestras para a', ''))
+      if (subjectsFromCache) {
+        console.log('Has eventData on cache. Does not need to request them.')
+        return setSubjectsOnStateAndIDB(subjectsFromCache)
+      }
     }
 
     effect()
--- src/pages/register-subject/register-subject.js
+++ src/pages/register-subject/register-subject.js
@@ -2,10 +2,19 @@ import React from 'react'
 
 function RegisterSubject () {
   return (
-    <form method='get' action='/'>
-      <label htmlFor='issueUrl'>URL completa da issue:</label>
-      <input type='text' name='issueUrl' />
-    </form>
+    <>
+      <h1>Assuntos mais votados</h1>
+      <form method='get' action='/'>
+        <label htmlFor='issueUrl'>URL completa da issue:</label>
+        <input type='text' name='issueUrl' />
+        <button type='submit'>Enviar</button>
+      </form>
+
+      <h3>Instruções de como esse projeto funciona:</h3>
+      <p>
+        <a href='https://github.com/fdaciuk/most-voted'>https://github.com/fdaciuk/most-voted</a>
+      </p>
+    </>
   )
 }
 
